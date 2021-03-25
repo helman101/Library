d598c872baad22ae7dd1e61d16c4c3af85d4c5da
helman101 <helmanandresortegon@gmail.com>
2021-03-25 10:14:21 +0100

    fix linter errors

--- assets/script.js
+++ assets/script.js
@@ -10,17 +10,45 @@ const newBookBtn = document.querySelector('#form');
 
 const Book = (title, author, pages, read) => {
   const changeRead = () => {
-    read = !read
+    read = !read;
   };
 
   const info = () => {
-    let alreadyRead = (read) ? 'already read' : 'not read yet';
-    (alreadyRead, title)
+    const alreadyRead = (read) ? 'already read' : 'not read yet';
     return `${title} by ${author}, ${pages} pages, ${alreadyRead}`;
   };
 
-  return {title, author, pages, read, info, changeRead};
+  return {
+    title, author, pages, read, info, changeRead,
+  };
 };
+
+const LibraryModule = (() => {
+  const saveLibrary = () => {
+    localStorage.lib = JSON.stringify(library);
+  };
+
+  const addBookToLibrary = () => {
+    const newBook = Book(title.value, author.value, pages.value, read.checked);
+
+    library.push(newBook);
+
+    saveLibrary();
+    displayHtmlModule.showBooks(); // eslint-disable-line
+  };
+
+  const loadLibrary = () => {
+    const books = JSON.parse(localStorage.lib);
+    for (let i = 0; i < books.length; i += 1) {
+      const prototype = Book(books[i].title, books[i].author, books[i].pages, books[i].read);
+      Object.assign(books[i], prototype);
+    }
+
+    return books;
+  };
+
+  return { saveLibrary, addBookToLibrary, loadLibrary };
+})();
 
 const BookModule = (() => {
   const removeBook = (id) => {
@@ -32,11 +60,11 @@ const BookModule = (() => {
   const changeRead = (button) => {
     const { id } = button.parentNode;
     const para = button.parentNode.querySelector('p');
-    library[id].changeRead()
+    library[id].changeRead();
     para.innerHTML = library[id].info();
     LibraryModule.saveLibrary();
   };
-  return {removeBook, changeRead};
+  return { removeBook, changeRead };
 })();
 
 const displayHtmlModule = (() => {
@@ -60,7 +88,7 @@ const displayHtmlModule = (() => {
       content.appendChild(changeReadBtn);
       content.appendChild(removeBtn);
       container.appendChild(content);
-    };
+    }
   };
 
   const showForm = () => {
@@ -68,34 +96,7 @@ const displayHtmlModule = (() => {
     button.classList.toggle('hidden');
   };
 
-  return {showBooks, showForm};
-})();
-
-const LibraryModule = (() => {
-  const saveLibrary = () => {
-    localStorage.lib = JSON.stringify(library);
-  }
-
-  const addBookToLibrary = () => {
-    const newBook = Book(title.value, author.value, pages.value, read.checked);
-
-    library.push(newBook);
-
-    saveLibrary();
-    displayHtmlModule.showBooks();
-  }
-
-  const loadLibrary = () => {
-    const books = JSON.parse(localStorage.lib);
-    for (let i = 0; i < books.length; i += 1) {
-      let prototype = Book(books[i].title, books[i].author, books[i].pages, books[i].read) 
-      Object.assign(books[i], prototype);
-    }
-    
-    return books;
-  }
-
-  return {saveLibrary, addBookToLibrary, loadLibrary}
+  return { showBooks, showForm };
 })();
 
 if (localStorage.lib) {
